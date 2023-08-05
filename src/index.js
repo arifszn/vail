24808d955aaa086e0920c322d9c35ce5f7ed177d
Ariful Alam <swazan.arif@gmail.com>
2023-08-05 09:16:03 +0600

    Execute the Docker Compose command

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const { dockerComposeCommand } = require('./console/docker-compose-command');
 const { initCommand } = require('./console/init-command');
 const { INIT } = require('./constants/commands');
 
@@ -7,6 +8,10 @@ const command = process.argv[2];
 
 if (command === INIT) {
   initCommand();
+} else if (command) {
+  // If the command is not "init", assume it's a Docker Compose command
+  dockerComposeCommand(process.argv);
 } else {
   console.error(`Unknown command: ${command}`);
+  process.exit(1);
 }
