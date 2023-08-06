d4684ef4ca7dbfff94d679997d15c6b7cfb9cd78
Ariful Alam <swazan.arif@gmail.com>
2023-08-06 12:08:08 +0600

    Add MySQL in the service offerings

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,21 +1,21 @@
 const MY_SQL = 'mysql';
-const MINIO = 'minio';
 
 const NODE_14 = '14';
 const NODE_16 = '16';
 const NODE_18 = '18';
 
-const SERVICES = [MY_SQL, MINIO];
+const SERVICES = [MY_SQL];
+
+const SERVICES_WITH_VOLUME = [MY_SQL];
 
 const NODE_VERSIONS = [NODE_14, NODE_16, NODE_18];
 
 // export all
 module.exports = {
-  MY_SQL,
-  MINIO,
   NODE_VERSIONS,
   NODE_14,
   NODE_16,
   NODE_18,
   SERVICES,
+  SERVICES_WITH_VOLUME,
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+const path = require('path');
+
 const {
   RED_COLOR_CODE,
   RESET_COLOR_CODE,
@@ -12,4 +16,55 @@ const displaySuccessMessage = (message) => {
   console.log(`${GREEN_COLOR_CODE}${message}${RESET_COLOR_CODE}`);
 };
 
-module.exports = { displayErrorMessage, displaySuccessMessage };
+const writeDockerComposeFileWithService = (
+  selectedServices,
+  availableVolumes,
+) => {
+  const dockerComposePath = path.resolve(process.cwd(), 'docker-compose.yml');
+
+  // Read the current docker-compose.yml file
+  const dockerComposeContent = fs.readFileSync(dockerComposePath, 'utf8');
+
+  // Parse the YAML content into a JavaScript object
+  const parsedCompose = yaml.load(dockerComposeContent);
+
+  // Check if 'volumes' property exists in 'parsedCompose' object
+  if (!parsedCompose.volumes) {
+    parsedCompose.volumes = {};
+  }
+
+  // Add selected services to the services section
+  selectedServices.forEach((service) => {
+    if (!(service in parsedCompose.services)) {
+      const serviceStubPath = `./node_modules/vail/stubs/${service}.stub`;
+      const serviceStubContent = fs.readFileSync(serviceStubPath, 'utf8');
+
+      const serviceStub = yaml.load(serviceStubContent);
+      parsedCompose.services[service] = serviceStub[service];
+    }
+  });
+
+  // Remove volumes if no services are selected
+  if (selectedServices.length === 0) {
+    delete parsedCompose.volumes;
+  } else {
+    // Add selected volumes to the volumes section
+    availableVolumes.forEach((volume) => {
+      if (!(volume in parsedCompose.volumes)) {
+        parsedCompose.volumes[`vail-${volume}`] = { driver: 'local' };
+      }
+    });
+  }
+
+  // Convert the JavaScript object back to YAML format
+  const updatedComposeContent = yaml.dump(parsedCompose);
+
+  // Write the updated YAML content to docker-compose.yml
+  fs.writeFileSync(dockerComposePath, updatedComposeContent, 'utf8');
+};
+
+module.exports = {
+  displayErrorMessage,
+  displaySuccessMessage,
+  writeDockerComposeFileWithService,
+};
